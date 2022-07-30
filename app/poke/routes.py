a7eb5f7647471e66192ee918d4ac0c4634a1e750
Michael Prather <mikeprather20@gmail.com>
2022-07-30 11:12:06 -0500

    new repository, first upload, and new structure

---
+++ app/poke/routes.py
@@ -0,0 +1,50 @@
+from flask import Blueprint, render_template, request
+from .forms import pokemonForm
+import requests
+
+#from app.models import Pokemon
+
+poke = Blueprint('poke', __name__, template_folder='poke_template')
+
+####################################
+
+@poke.route('/pokemon', methods = ['GET', 'POST'])
+def pokedex():
+    form = pokemonForm()
+    my_dict = {}
+
+    if request.method == "POST":
+        poke_name = form.name.data
+
+        url = f"https://pokeapi.co/api/v2/pokemon/{poke_name}"
+        res = requests.get(url)
+        if res.ok:
+            data = res.json()
+            my_dict = {
+                'name': data['name'],
+                'ability': data['abilities'][0]['ability']['name'],
+                'img_url': data['sprites']['front_shiny'],
+                'hp': data['stats'][0]['base_stat'],
+                'attack': data['stats'][1]['base_stat'],
+                'defense': data['stats'][2]['base_stat']
+            }
+        else:
+            return "ERROR"
+
+    return render_template('pokemon.html', form = form, pokemon = my_dict)
+
+
+################################
+
+
+@poke.route('/login')
+def login():
+    return render_template('login.html')
+
+
+################################
+
+
+@poke.route('/register')
+def register():
+    return render_template('register.html')
