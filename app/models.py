5c6c5cbb4e63d9f3f5d77af5a43485c17edcd3f3
Michael Prather <mikeprather20@gmail.com>
2022-08-02 10:01:03 -0500

    fixed models

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), nullable=False unique=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
     first_name = db.Column(db.String(150), nullable=False)
     last_name = db.Column(db.String(150), nullable=False)
     email = db.Column(db.String(150), nullable=False, unique=True)
